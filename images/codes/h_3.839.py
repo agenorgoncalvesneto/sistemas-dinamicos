59cfd42506118bfba4447e70045ce4733aa21480
agenorgoncalvesneto <agenorgoncalvesneto@gmail.com>
2020-11-14 03:02:07 -0300

    remove book and update multiple

--- images/codes/h_3.839.py
+++ images/codes/h_3.839.py
@@ -1,17 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def T(x, n=1):
+def h(x, mu, n=1):
     y = x
     for i in range(1, n+1):
-        y = 1 - np.abs(2*y - 1)
+        y = mu*y*(1-y)
     return y
 
 x = np.linspace(start=0, stop=1, num=10000)
+y = h(x, 3.839, n=3)
 
-for k in range(1, 4):
-    y = T(x, k)
-    plt.subplot(1, 3, k)
-    plt.plot(x, y, color='black')
-
+plt.plot(x, y, color='black')
+plt.plot(x, x, color='black', linewidth=0.5)
 plt.show()
--- images/codes/tent-map.py
+++ images/codes/tent-map.py
@@ -1,19 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def h(x, mu, n=1):
+def T(x, n=1):
     y = x
     for i in range(1, n+1):
-        y = mu*y*(1-y)
+        y = 1 - np.abs(2*y - 1)
     return y
 
-x = np.linspace(start=0.5, stop=0.8, num=10000)
+x = np.linspace(start=0, stop=1, num=10000)
 
-mu = [2.9, 3, 3.1]
 for k in range(1, 4):
-    y = h(x, mu[k-1], 2)
+    y = T(x, k)
     plt.subplot(1, 3, k)
     plt.plot(x, y, color='black')
-    plt.plot(x, x, color='black')
-    
+    plt.plot(x, x, color='black', linewidth=0.5)
+
 plt.show()
